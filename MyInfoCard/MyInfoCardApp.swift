e4aa8d9d1a86ba7b1a01a00229c03e0415ab2fac
Emir Türk <turkemir2152@gmail.com>
2023-07-22 21:05:32 +0300

    Initial Commit

---
+++ MyInfoCard/MyInfoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyInfoCardApp.swift
+//  MyInfoCard
+//
+//  Created by Emir Türk on 22.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyInfoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
